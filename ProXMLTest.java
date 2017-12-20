3a7de6ebdff853d063757445c3428245c23df63b
vincenza <vincy1701@hotmail.it>
2017-12-20 09:06:07 +0100

    hh

---
+++ ProXMLTest.java
@@ -0,0 +1,16 @@
+package proxml.test;
+
+public class ProXMLTest{
+	
+	private void testProXml(){
+		//XMLElement element = XMLElement.loadElementFrom(this.getClass().getResource("pro.xml"));
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args){
+		new ProXMLTest().testProXml();
+	}
+
+}
